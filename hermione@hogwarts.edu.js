a6f9729a7a5687d8179c760d6fea211df6127ec8
CreativeUrge <adamtagee@gmail.com>
2016-02-09 12:46:41 -0700

    Create hermione@hogwarts.edu.js

---
+++ hermione@hogwarts.edu.js
@@ -0,0 +1,97 @@
+{
+	"name": "Hermione",
+	"email": "hermione@hogwarts.edu",
+	"password": "321",
+	"stripeID": null,
+	"memberStatus": "sample",
+	"joinDate": "2/3/2016",
+	"activityStatus": "Logged Out",
+	"lastActiveDate": "2/4/2016",
+	"trialStartDate": null,
+	"trialEndDate": null,
+	"hasHadTrial": false,
+	"hasHadTour": true,
+	"totalProjectsCreated": 6,
+	"profiles": [
+		{
+			"profileName": "School Projects",
+			"projects": [
+				{
+					"projectId": 0,
+					"projectTitle": "Potions",
+					"dueDate": "2/14/2016",
+					"timeEstimate": 10,
+					"timeSpent": 0,
+					"timeSpentToday": 0,
+					"suggestedAmountToday": 1.5,
+					"projectStatus": "okay"
+				},
+				{
+					"projectId": 1,
+					"projectTitle": "Charms",
+					"dueDate": "3/5/16",
+					"timeEstimate": 13,
+					"timeSpent": 0,
+					"timeSpentToday": 0,
+					"suggestedAmountToday": 0.6666666666666666,
+					"projectStatus": "okay"
+				},
+				{
+					"projectId": 2,
+					"projectTitle": "Arithmancy Final Project",
+					"dueDate": "3/5/16",
+					"timeEstimate": 50,
+					"timeSpent": 0,
+					"timeSpentToday": 0,
+					"suggestedAmountToday": 2.3333333333333335,
+					"projectStatus": "okay"
+				}
+			],
+			"workSchedule": {
+				"mo": "5",
+				"tu": "7",
+				"we": "5",
+				"th": "10",
+				"fr": "3",
+				"sa": "12",
+				"su": "0"
+			},
+			"vacationDays": []
+		},
+		{
+			"profileName": "Personal Projects",
+			"projects": [
+				{
+					"projectId": 0,
+					"projectTitle": "Essay on History of Hogwarts",
+					"dueDate": "2/29/16",
+					"timeEstimate": 30,
+					"timeSpent": 0,
+					"timeSpentToday": 0,
+					"suggestedAmountToday": 0,
+					"projectStatus": "okay"
+				},
+				{
+					"projectId": 1,
+					"projectTitle": "Dragon Research",
+					"dueDate": "3/17/2016",
+					"timeEstimate": 25,
+					"timeSpent": 0,
+					"timeSpentToday": 0,
+					"suggestedAmountToday": 0,
+					"projectStatus": "okay"
+				}
+			],
+			"workSchedule": {
+				"mo": "0",
+				"tu": "0",
+				"we": "0",
+				"th": "0",
+				"fr": "0",
+				"sa": "5",
+				"su": "5"
+			},
+			"vacationDays": []
+		}
+	]
+}
